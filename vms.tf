18ac7b69fcd3bc9e9c75a233576812c139354c59
superg28 <gharington@gmail.com>
2023-03-15 20:46:30 +0200

    added pip to vm2

--- vms.tf
+++ vms.tf
@@ -20,6 +20,7 @@ resource "azurerm_network_interface" "nic-two" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet-two.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip-two.id
   }
 }
 
