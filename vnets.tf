d55be55f2b766ceaa056df1fb55c2429254acf0a
superg28 <gharington@gmail.com>
2023-03-21 20:46:46 +0200

    resource clean up

--- vnets.tf
+++ vnets.tf
@@ -21,7 +21,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "vnet-two-link" {
   virtual_network_id    = azurerm_virtual_network.vnet-two.id
   registration_enabled  = true
 }
-*/
+
 # VNets
 resource "azurerm_virtual_network" "vnet-one" {
   name                = "test-vnet-one"
@@ -29,14 +29,14 @@ resource "azurerm_virtual_network" "vnet-one" {
   resource_group_name = azurerm_resource_group.networking-rg.name
   address_space       = ["10.0.0.0/16"]
 }
-/*
+
 resource "azurerm_virtual_network" "vnet-two" {
   name                = "test-vnet-two"
   location            = azurerm_resource_group.networking-rg.location
   resource_group_name = azurerm_resource_group.networking-rg.name
   address_space       = ["10.1.0.0/16"]
 }
-*/
+
 # Subnets
 resource "azurerm_subnet" "subnet-one" {
   name                 = "test-subnet-one"
@@ -44,7 +44,7 @@ resource "azurerm_subnet" "subnet-one" {
   virtual_network_name = azurerm_virtual_network.vnet-one.name
   address_prefixes     = ["10.0.1.0/24"]
 }
-/*
+
 resource "azurerm_subnet" "subnet-two" {
   name                 = "test-subnet-two"
   resource_group_name  = azurerm_resource_group.networking-rg.name
@@ -66,7 +66,7 @@ resource "azurerm_subnet" "subnet-two" {
 #   virtual_network_name = azurerm_virtual_network.vnet-two.name
 #   address_prefixes     = ["10.1.2.0/24"]
 # }
-*/
+
 # Public IPs
 # for VMs
 resource "azurerm_public_ip" "pip-one" {
