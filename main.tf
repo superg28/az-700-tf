bde2560aba0927fb6e9d323b54f1720f07c1f91e
superg28 <gharington@gmail.com>
2023-03-22 21:59:57 +0200

    express route prep

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 # Setup the resource group
 resource "azurerm_resource_group" "networking-rg" {
   name     = "az-networking-cert-rg"
-  location = "southafricanorth"
+  location = var.rg_location
 }
